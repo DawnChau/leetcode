87689e3cd464a9050ce1190bbababc6c779fabf8
dawnchau <czcacqcc@qq.com>
2019-02-26 16:40:27 +0800

    2019年02月26日 更新gitignore

---
+++ src/Pro_020.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+/**
+ * 括号匹配问题
+ */
+public class Pro_020 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (Character c : s.toCharArray()) {
+            switch (c) {
+                case '{':
+                    stack.push('}');
+                    break;
+                case '[':
+                    stack.push(']');
+                    break;
+                case '(':
+                    stack.push(')');
+                    break;
+                default:
+                    // 当没有压入或者上一个压入的不对，则为错
+                    if(stack.isEmpty() || stack.pop()!=c)
+                        return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
---
+++ src/Pro_021.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+
+public class Pro_021 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+
+        // 头 和 指针
+        ListNode head = null;
+        ListNode currentPoint = null;
+        ListNode l1Point = null;
+        ListNode l2Point = null;
+
+        if(l1.val < l2.val){
+            head = l1;
+            l1Point = l1.next;
+            l2Point = l2;
+        }
+        else{
+            head = l2;
+            l1Point = l1;
+            l2Point = l2.next;
+        }
+
+        currentPoint = head;
+
+        while(l1Point!=null && l2Point!=null){
+            if(l1Point.val < l2Point.val){
+                currentPoint.next = l1Point;
+                currentPoint = currentPoint.next;
+                l1Point = l1Point.next;
+            }else{
+                currentPoint.next = l2Point;
+                currentPoint = currentPoint.next;
+                l2Point = l2Point.next;
+            }
+        }
+
+        if(l1Point == null){
+            currentPoint.next = l2Point;
+        }else if(l2Point == null){
+            currentPoint.next = l1Point;
+        }
+
+        return head;
+    }
+}
+
+
