5605b934ee36473e5458e8cb8561423027577d48
dawnchau <czcacqcc@qq.com>
2019-02-28 15:26:43 +0800

    2019年02月28日 今天刷了12道啦

---
+++ src/Pro_171.java
@@ -0,0 +1,19 @@
+/**
+ * 26进制转10进制
+ */
+public class Pro_171 {
+    public static int titleToNumber(String s) {
+        int ans = 0;
+        for(int i = 0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(Character.isAlphabetic(c)){
+                ans = ans*26+c-'A'+1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(titleToNumber("AB"));
+    }
+}
---
+++ src/Pro_217.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 查找重复元素
+ */
+public class Pro_217 {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0;i<nums.length;i++){
+            if(!set.add(nums[i]))
+                return true;
+        }
+        return false;
+    }
+}
