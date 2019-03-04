504aacdbbc8ae7867956e0637b12391343529b2a
dawnchau <czcacqcc@qq.com>
2019-03-04 20:59:21 +0800

    2019年03月04日 今天10道刷完啦

--- src/Pro_029.java
+++ src/Pro_029.java
@@ -3,37 +3,59 @@
  */
 public class Pro_029 {
     public static int divide(int dividend, int divisor) {
+        // 记录正负
         boolean positive = true;
-        if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0))
-            positive = false;
+
+        // 扩展为long
+        long longDivisor = divisor;
+        long longDividend = dividend;
+
+        // 特殊情况处理
         if (dividend == Integer.MIN_VALUE && divisor == -1)
-            dividend = Integer.MIN_VALUE + 1;
-        dividend = Math.abs(dividend);
-        divisor = Math.abs(divisor);
-        if (dividend == divisor)
+            return Integer.MAX_VALUE;
+
+        // 正负判断
+        if ((longDividend < 0 && longDivisor > 0) || (longDividend > 0 && longDivisor < 0))
+            positive = false;
+
+        // 全部取正
+        longDividend = Math.abs(longDividend);
+        longDivisor = Math.abs(longDivisor);
+
+        // 相等及除数是1的判断
+        if (longDividend == longDivisor)
             return positive ? 1 : -1;
-        if (divisor == 1)
+        if (longDivisor == 1)
             return positive ? dividend : -1 * dividend;
 
         int res = 0;
         int count = 0;
-        int originDivisor = divisor;
-        while (dividend > divisor) {
-            divisor <<= 1;
+        long originDivisor = longDivisor;
+        while (longDividend > longDivisor) {
+            // 除数每次乘以2
+            longDivisor <<= 1;
+            // 记录幂数
             count++;
-            if (divisor > dividend) {
-                divisor >>= 1;
+            if (longDivisor > longDividend) {
+                // 超过了，回退一下
+                longDivisor >>= 1;
                 count--;
-                dividend = dividend - divisor;
-                divisor = originDivisor;
+                // 剩余的差重新作为被除数
+                longDividend = longDividend - longDivisor;
+                // 除数恢复成最初的
+                longDivisor = originDivisor;
                 res += Math.pow(2, count);
                 count = 0;
+            } else if (longDividend == longDivisor) {
+                // 恰好相等直接返回
+                res += Math.pow(2, count);
+                break;
             }
         }
         return positive ? res : -1 * res;
     }
 
     public static void main(String[] args) {
-        System.out.println(divide(-2147483648, -1));
+        System.out.println(divide(-2147483648, 2));
     }
 }
--- src/Pro_033.java
+++ src/Pro_033.java
@@ -1,5 +1,5 @@
 /**
- * 旋转后数组的二分查找法
+ * 旋转后数组的二分查找法--（难啊）
  */
 public class Pro_033 {
     public static int search(int[] nums, int target) {
@@ -7,22 +7,56 @@ public class Pro_033 {
         int right = nums.length - 1;
 
 
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
+
+
+            if (left == right && nums[left] != target)
+                break;
+            // 忽略下文中的等号判断
             if (nums[mid] == target)
                 return mid;
-            else if (target > nums[right] || target < nums[mid]) {
-                // 如果target比最右边的数还大，那么证明在左边里继续找
-                right = mid - 1;
-            } else if (target < nums[left] || target > nums[mid]) {
-                left = mid + 1;
+            if (nums[right] == target)
+                return right;
+            if (nums[left] == target)
+                return left;
+
+            // 先判断哪个区是增序的
+            if (nums[mid] > nums[left]) {
+                // 左半区是增序的
+                if (target > nums[mid]) {
+                    // 属于右半区
+                    left = mid + 1;
+                } else {
+                    if (target < nums[left]) {
+                        // 属于右半区
+                        left = mid + 1;
+                    } else {
+                        // 属于左半区
+                        right = mid - 1;
+                    }
+                }
+            } else {
+                // 左半区不是递增的
+                if (target < nums[mid]) {
+                    // 属于左半区
+                    right = mid - 1;
+                } else {
+                    if (target > nums[right]) {
+                        // 属于左半区
+                        right = mid - 1;
+                    } else {
+                        // 属于右半区
+                        left = mid + 1;
+                    }
+                }
             }
         }
         return -1;
     }
 
     public static void main(String[] args) {
-        int[] a = {4, 5, 6, 7, 0, 1, 2};
+        int[] a = {3, 1};
         System.out.println(search(a, 0));
     }
 }
